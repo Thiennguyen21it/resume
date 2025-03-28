5ca9f5feebaf98bd1d22ac4feb28fbed6e482575
thiennguyen <thiennh.21it@vku.udn.vn>
2025-03-28 13:40:22 +0700

    update cv in 1 page pdf

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t sb2nov/latex .
-docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex nguyen_hoan_thien_resume.text 
+docker build -t latex .
+docker run --rm -i -v "$PWD":/data latex pdflatex nguyen_hoan_thien_resume.tex
